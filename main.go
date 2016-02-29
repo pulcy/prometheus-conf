36e9b062081d584b7c66aa8f92facdd34df1b3f9
Ewout Prangsma <ewout@prangsma.net>
2016-02-29 14:50:45 +0100

    Initial import

---
+++ main.go
@@ -0,0 +1,87 @@
+// Copyright (c) 2016 Pulcy.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/op/go-logging"
+	"github.com/spf13/cobra"
+
+	"github.com/pulcy/prometheus-conf/service"
+)
+
+const (
+	projectName       = "prometheus-conf"
+	defaultConfigPath = "./prometheus.yml"
+)
+
+var (
+	defaultLoopDelay = time.Second * 30
+
+	projectVersion = "dev"
+	projectBuild   = "dev"
+)
+
+var (
+	cmdMain = &cobra.Command{
+		Use:   projectName,
+		Short: "Build prometheus configuration file for a Pulcy cluster",
+		Run:   cmdMainRun,
+	}
+	log   = logging.MustGetLogger(projectName)
+	flags struct {
+		service.ServiceConfig
+	}
+)
+
+func init() {
+	logging.SetFormatter(logging.MustStringFormatter("[%{level:-5s}] %{message}"))
+	cmdMain.Flags().StringVar(&flags.ConfigPath, "config-path", defaultConfigPath, "Path of the generated config file")
+	cmdMain.Flags().BoolVar(&flags.Once, "once", false, "If set, the config ill be generated only once")
+	cmdMain.Flags().DurationVar(&flags.LoopDelay, "loop-delay", defaultLoopDelay, "Time to wait before rebuilding the config file")
+}
+
+func main() {
+	cmdMain.Execute()
+}
+
+func cmdMainRun(cmd *cobra.Command, args []string) {
+	s := service.NewService(flags.ServiceConfig, service.ServiceDependencies{
+		Log: log,
+	})
+	if err := s.Run(); err != nil {
+		Exitf("Config creation failed: %#v", err)
+	}
+}
+
+func Exitf(format string, args ...interface{}) {
+	if !strings.HasSuffix(format, "\n") {
+		format = format + "\n"
+	}
+	fmt.Printf(format, args...)
+	os.Exit(1)
+}
+
+func def(envKey, defaultValue string) string {
+	s := os.Getenv(envKey)
+	if s == "" {
+		s = defaultValue
+	}
+	return s
+}
---
+++ service/service.go
@@ -0,0 +1,83 @@
+// Copyright (c) 2016 Pulcy.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package service
+
+import (
+	"io/ioutil"
+	"time"
+
+	"github.com/go-yaml/yaml"
+	"github.com/op/go-logging"
+)
+
+type ServiceConfig struct {
+	ConfigPath string
+	Once       bool
+	LoopDelay  time.Duration
+}
+
+type ServiceDependencies struct {
+	Log *logging.Logger
+}
+type Service struct {
+	ServiceConfig
+	ServiceDependencies
+}
+
+func NewService(config ServiceConfig, deps ServiceDependencies) *Service {
+	return &Service{
+		ServiceConfig:       config,
+		ServiceDependencies: deps,
+	}
+}
+
+// Build the config once of continuously
+func (s *Service) Run() error {
+	for {
+		err := s.runOnce()
+		if err != nil {
+			s.Log.Errorf("runOnce failed: %#v", err)
+		}
+		if s.Once {
+			return maskAny(err)
+		}
+		time.Sleep(s.LoopDelay)
+	}
+	return nil
+}
+
+func (s *Service) runOnce() error {
+	// Build configuration object
+	config, err := s.createConfig()
+	if err != nil {
+		return maskAny(err)
+	}
+
+	// Save config
+	raw, err := yaml.Marshal(config)
+	if err != nil {
+		return maskAny(err)
+	}
+	if err := ioutil.WriteFile(s.ConfigPath, raw, 0755); err != nil {
+		return maskAny(err)
+	}
+
+	return nil
+}
+
+func (s *Service) createConfig() (PrometheusConfig, error) {
+	config := PrometheusConfig{}
+	return config, nil
+}
